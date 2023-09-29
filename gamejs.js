0b31a93d4f112ea4d10a86691f67557c7a3b9a05
Your Name <youremail@example.com>
2023-09-29 23:08:50 +0530

    new day

--- gamejs.js
+++ gamejs.js
@@ -5,10 +5,10 @@ function shareOnTwitter() {
     
     if (attempts === 0) {
         // If no attempts left, display a failure message
-        shareMessage = "I failed to guess the movie name in 4 attempts! Can you do better?  #GuessTheFlick  https://guess-the-flick.netlify.app/";
+        shareMessage = "Guess the flick Day 3:-I failed to guess the movie name in 4 attempts! Can you do better?  #GuessTheFlick  https://guess-the-flick.netlify.app/";
     } else {
     // Define the sharing message
-        shareMessage = "Guess the flick Day 2:-I guessed the movie name in " + (4 - attempts + 1) + " attempts! Can you beat me? #GuessTheFlick  https://guess-the-flick.netlify.app/";
+        shareMessage = "Guess the flick Day 3:-I guessed the movie name in " + (4 - attempts + 1) + " attempts! Can you beat me? #GuessTheFlick  https://guess-the-flick.netlify.app/";
     }
 
     // Twitter sharing URL
@@ -65,14 +65,14 @@ const aboutIcon = document.getElementById("about-icon");
 const aboutDescription = document.getElementById("about-description");
 
 // Replace these placeholders with actual values
-const initialHero = "S";
-const initialHeroine = "S";
-const initialMovieName = "A";
-const correctAnswer = "Anaga Anaga O Dheerudu";
-const clue = "Nimma dhanimma kanipinchindha jejamma";
+const initialHero = "A";
+const initialHeroine = "R";
+const initialMovieName = "K";
+const correctAnswer = "Kotha janta";
+const clue = "Hero works for a TV channel";
 // Manually provide YouTube links for male and female players
-const maleVideoUrl = "https://www.youtube.com/embed/CiOq8mfMHNc";
-const femaleVideoUrl = "https://www.youtube.com/embed/Xl3hk5DK9LQ";
+const maleVideoUrl = "https://www.youtube.com/embed/yXGdFDyTjDc";
+const femaleVideoUrl = "https://www.youtube.com/embed/-Dvn4OPgRJA";
 
 let attempts = 4;
 
@@ -140,9 +140,9 @@ submitButton.addEventListener("click", () => {
         correctSound.play();
 
         // Reveal all letters for hero, heroine, and movie name
-        heroInitialElement.textContent = initialHero + "iddarth";
-        heroineInitialElement.textContent = initialHeroine + "hruti haasan";
-        movieNameInitialElement.textContent = initialMovieName + "naga Anaga O Dheerudu";
+        heroInitialElement.textContent = initialHero + "llu sirish";
+        heroineInitialElement.textContent = initialHeroine + "egina cassandra";
+        movieNameInitialElement.textContent = initialMovieName + "otha janta";
 
         // Update the result message
         resultElement.textContent = "Congratulations! You guessed it correctly.";
@@ -189,9 +189,9 @@ submitButton.addEventListener("click", () => {
 
         if (attempts === 0) {
             // No more attempts left, display the answers and a message
-            heroInitialElement.textContent = initialHero + "iddarth";
-            heroineInitialElement.textContent = initialHeroine + "hruti haasan";
-            movieNameInitialElement.textContent = initialMovieName + "naga Anaga O Dheerudu";
+            heroInitialElement.textContent = initialHero + "llu sirish";
+            heroineInitialElement.textContent = initialHeroine + "egina cassandra";
+            movieNameInitialElement.textContent = initialMovieName + "otha janta";
             resultElement.textContent = "You lost. Better luck tomorrow!";
             resultElement.style.color = "red";
             submitButton.disabled = true; // Disable the submit button
